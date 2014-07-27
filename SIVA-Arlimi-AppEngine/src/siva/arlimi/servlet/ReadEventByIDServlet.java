61dfe5534990d49264f68c3f5a39af2b890ded0e
eugene0204 <eugene204@gmail.com>
2014-07-27 23:17:16 +0900

    20140727

--- SIVA-Arlimi-AppEngine/src/siva/arlimi/servlet/ReadEventByIDServlet.java
+++ SIVA-Arlimi-AppEngine/src/siva/arlimi/servlet/ReadEventByIDServlet.java
@@ -2,16 +2,24 @@ package siva.arlimi.servlet;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.labs.repackaged.org.json.JSONArray;
-import com.google.appengine.labs.repackaged.org.json.JSONException;
-import com.google.appengine.labs.repackaged.org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import siva.arlimi.database.DatabaseConnection;
+import siva.arlimi.util.EventUtil;
 
 public class ReadEventByIDServlet extends HttpServlet
 {
@@ -20,70 +28,99 @@ public class ReadEventByIDServlet extends HttpServlet
 	 */
 	private static final long serialVersionUID = -2008857857750225485L;
 
-
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException 
+			throws ServletException, IOException
 	{
 		System.out.println("doGet");
-		
+
 	}
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException 
+			throws ServletException, IOException
 	{
 		System.out.println("readEventID post");
 		
+		StringBuilder sb = readRequest(req);
+		JSONObject events = conversionToJson(sb);
+		String[] eventIds = readIds(events);
+		JSONArray result = readDB(eventIds);
+		
+		sendResult(result, resp);
+
+	}
+	
+	private void sendResult(JSONArray result,HttpServletResponse resp) throws UnsupportedEncodingException
+	{
+		OutputStreamWriter writer;
+		try
+		{
+			writer = new OutputStreamWriter(resp.getOutputStream(),"utf8");
+			writer.write(result.toString());
+			writer.close();
+			
+		}
+		catch (IOException e1)
+		{
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+	
+	private StringBuilder readRequest(HttpServletRequest req)
+	{
 		StringBuilder sb = new StringBuilder();
 		String line = "";
-		BufferedReader bf;	
-		JSONArray jsonArray = null;
-	
+		BufferedReader bf;
+
 		try
 		{
 			bf = req.getReader();
-			while( null != (line = bf.readLine() ))
+			while (null != (line = bf.readLine()))
 			{
 				sb.append(line);
 			}
 		}
-		catch(Exception e) { }
-		
-		System.out.println("sb: " + sb.toString());
-	
-		JSONObject json = new JSONObject();
-		
-		try 
-		{
-			if(!sb.toString().isEmpty())
-			{
-				json = new JSONObject(sb.toString()); 
-			}
-			else
-			{
-				System.out.println("StringBuilder is empty");
-			}
-			
-		}
-		catch (JSONException e)
+		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
 		
-		//Get Ids from JsonArray
-		String[] eventIds = readIds(json);
-		//readDB(eventIds);
-		
+		return sb;
 	}
 	
+	private JSONObject conversionToJson(StringBuilder sb)
+	{
 	
+		JSONObject json = new JSONObject();
+
+		try
+		{
+			if (!sb.toString().isEmpty())
+			{
+				json = new JSONObject(sb.toString());
+			} 
+			else
+			{
+				System.out.println("StringBuilder is empty");
+			}
+
+		} catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return json;
+	}
+
 	private String[] readIds(JSONObject json)
 	{
-		int len = json.length();
+		int len = json.length() - 1;
 		String[] eventIds = new String[len];
-		
-		for(int i = 0; i < len - 1; i++)
+
+		for (int i = 0; i < len; i++)
 		{
 			try
 			{
@@ -93,76 +130,73 @@ public class ReadEventByIDServlet extends HttpServlet
 			{
 				e.printStackTrace();
 			}
-			
 		}
-		
+
 		return eventIds;
 	}
 
 	private JSONArray readDB(String[] eventId)
 	{
 		Connection conn = null;
-		JSONArray eventArray = new JSONArray();
-		
+		JSONArray jsonArray = new JSONArray();
+
 		String query = makeQuery(eventId);
-		
-		/*
+		 	
 		try
 		{
 			conn = DatabaseConnection.getConnection();
 			Statement stmt = conn.createStatement();
-			//String query = "select * from event;"; 
 			ResultSet rs = stmt.executeQuery(query); 
-			
 			
 			while(rs.next())
 			{
-				
 				JSONObject event = new JSONObject();
-				try
-				{
-					event.put(EventUtil.EVENT_ID, Integer.valueOf(rs.getInt("id")));
-					
-					event.put(EventUtil.EMAIL, rs.getString("owner_email"));
-					event.put(EventUtil.EVENT_CONTENTS, rs.getString("event_contents"));
-					event.put(EventUtil.EVENT_LATITUDE, rs.getString("event_latitude"));
-					event.put(EventUtil.EVENT_LONGITUDE, rs.getString("event_longitude"));
-					
-				}
-				catch (JSONException e) 
-				{
-					e.printStackTrace();
-				}
 				
-				//eventArray.add(event); 
-				eventArray.put(event);
+				event.put(EventUtil.EVENT_ID, Integer.valueOf(rs.getInt("id")));
+
+				event.put(EventUtil.EMAIL, rs.getString("owner_email"));
+				event.put(EventUtil.EVENT_CONTENTS,
+						rs.getString("event_contents"));
+				event.put(EventUtil.EVENT_LATITUDE,
+						rs.getString("event_latitude"));
+				event.put(EventUtil.EVENT_LONGITUDE,
+						rs.getString("event_longitude"));
+				
+				jsonArray.put(event);
+
 			}
 			
-		} catch (SQLException e)
+		} 
+		catch (SQLException e)
 		{
 			e.printStackTrace();
-		} catch(NullPointerException e)
+		} catch (NullPointerException e)
 		{
 			e.printStackTrace();
-		} */
+		} catch (JSONException e)
+		{
+			e.printStackTrace();
+		}
 		
-		return eventArray;
+		return jsonArray;
 	}
 
 	private String makeQuery(String[] eventId)
 	{
-		StringBuilder sb = new StringBuilder
-				("selcet * from event where id in(");
-		
-		for(int i = 0; i < eventId.length; i++)
+		StringBuilder sb = new StringBuilder("select * from event where id in(");
+
+		for (int i = 0; i < eventId.length; i++)
 		{
-			sb.append(eventId[i]);
-			if( !((eventId.length -1) == i))
-				sb.append(",");
+			if (null != eventId[i])
+			{
+				sb.append(eventId[i]);
+				if (!((eventId.length - 1) == i))
+					sb.append(",");
+			}
 		}
 		sb.append(")");
-		
+
 		return sb.toString();
 	}
-	
+
 }
